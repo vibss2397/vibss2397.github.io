3728d9026d0c25d697688c1b52cc2f0a68636bd6
vibhu <vibhu2397@gmail.com>
2020-01-25 19:47:28 -0500

    minor fixes

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -24,25 +24,46 @@
     }
   });
   var classes_to_add = ['g-1', 'g-2', 'g-3', 'g-4','g-5','g-6'];
-  var titles=['Generating rick n morty subtitles via machine learning', 'Path Finding Algorithms', 'Playing frozen lake via qlearning', 'SeeThings, Shazam for things', 'classifying facial emotions', 'Generating superheroes using deep learning', 'teaching a bot to play atari games']
-  var pathtolink=['rnn','path','qlearn','shazam','emoji','gan','reinforce'];
+  var titles=['Generating rick n morty subtitles via machine learning', 'Greedy vs Heuristic', 'Playing frozen lake via qlearning', 'Shazam for things', 'classifying facial emotions', 'Generating superheroes using deep learning', 'teaching a bot to play atari games']
+  var pathtolink=['char-RNN','Path-Finding-Algorithms','Q-Learning','See-Things','Face-Emoji','Superhero-GAN','Policy-Gradients'];
+  var pathtoImages = ['./img/gif/charRnn.gif', './img/gif/astar.gif', './img/gif/qlearn.gif', './img/seething/main.gif', './img/face2emoji/back.png', './img/gan/marvel3.jpg', './img/gif/atari.gif']
+  var bleh = [0, 1, 2, 3, 4, 5, 6];
+  var choice = [];
+  for(let i = 0;i<7;i++){
+    let tem = Math.floor(Math.random() * bleh.length);
+    choice.push(bleh[tem]);
+    bleh.splice(tem, 1);    
+  }
+  
   for (var i =0;i<7;i++){
-    var choice= Math.floor(Math.random() * Math.floor(6));
     $('.owl-container-ele').append(
-        '<a href="./project/template.html?name='+pathtolink[i]+'" style="color:#fff;text-decoration:none;overflow: hidden">'+
+        '<a href="./project/template.html?name='+pathtolink[choice[i]]+'" style="color:#fff;text-decoration:none;overflow: hidden">'+
         '<div class="item mx-auto card grad-card project-card-'+i+' "  data-tilt data-tilt-max="10"  data-tilt-glare data-tilt-max-glare="0.8">'+
-            '<h4 class="p-4" style="z-index: 99" >'+titles[i]+'</h4>'+
-            '<div class="card  " style="position: absolute;width: 100%;height: 100%;z-index: 22;mix-blend-mode: luminosity;opacity: 0.7" id="'+pathtolink[i]+'">'+
+            '<h4 class="p-4" style="z-index: 99" >'+titles[choice[i]]+'</h4>'+
+            // '<img class="img-fluid mx-auto my-auto" style="z-index:999;width:40% !important;" id="image" src="'+pathtoImages[i]+'" >'+
+            '<div class="card  " style="position: absolute;width: 100%;height: 100%;z-index: 22;mix-blend-mode: luminosity;opacity: 0.7;" id="'+pathtolink[choice[i]]+'">'+
             '</div>'+
 
           '</div></a>'   
     );
-    $('.project-card-'+i).addClass(classes_to_add[i%6]);
-    var pattern = pathtolink[i];
-      $('#'+pathtolink[i]).geopattern(pattern);
+    $('.project-card-'+i).addClass(classes_to_add[choice[i]%6]);
+    var pattern = pathtolink[choice[i]];
+      $('#'+pathtolink[choice[i]]).geopattern(pattern);
 
   }
-  
+  for(let i = 0;i<3;i++){
+    let rand = Math.floor(Math.random()*pathtolink.length);
+    $('.featured-project').append('<a href="./project/template.html?name=gan" style="color:#fff;text-decoration:none;overflow: hidden">'+
+    '<div class="card masthead-card tilt-card mx-auto   "  data-tilt data-tilt-glare data-tilt-max-glare="0.5" data-tilt-max="5" style="transform-style: preserve-3d;height:auto">'+
+      '<div class="topic px-3 pt-3">'+
+        '<h3 class="m-0">'+pathtolink[rand]+'</h3>'+
+        '<h6 style="font-weight: 400">('+titles[rand]+')</h6>'+
+      '</div>'+
+      '<div class="card grad-card mt-auto p-3" style="transform: translateZ(20px)"><img class="img-fluid mx-auto my-auto" id="image" src="'+pathtoImages[rand]+'" >'+
+      '</div>'+
+    '</div></a>')
+  }
+
   for (var i=1;i<=3;i++){
     let r = Math.random().toString(36).substring(2, 10);
     $('.research-card-'+i.toString()).geopattern(r);
